a9524e64ff46003d3d1c551b7ade1b36c99594ff
Ima <fatimatul@sage42.net>
2019-07-02 12:24:41 +0800

    add cordova

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -2,13 +2,11 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
+        <q-btn flat
           dense
           round
           @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-        >
+          aria-label="Menu">
           <q-icon name="menu" />
         </q-btn>
 
@@ -20,14 +18,14 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
+    <q-drawer v-model="leftDrawerOpen"
       bordered
-      content-class="bg-grey-2"
-    >
+      content-class="bg-grey-2">
       <q-list>
         <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="div" to="/">
+        <q-item clickable
+          tag="div"
+          to="/">
           <q-item-section avatar>
             <q-icon name="school" />
           </q-item-section>
@@ -36,7 +34,9 @@
             <q-item-label caption>home</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="div" to="/troubleshoot">
+        <q-item clickable
+          tag="div"
+          to="/troubleshoot">
           <q-item-section avatar>
             <q-icon name="rss_feed" />
           </q-item-section>
